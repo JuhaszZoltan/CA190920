1a6a721bcb51b543c6523789ff1be729b02726db
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2019-09-20 12:25:37 +0200

    mostmár tud csillagokat

--- CA190920/Program.cs
+++ CA190920/Program.cs
@@ -8,6 +8,7 @@ namespace CA190920
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
             Console.WriteLine("Szia, hogy hívnak?");
@@ -24,6 +25,17 @@ namespace CA190920
                 Console.WriteLine(":(");
                 Console.WriteLine("Akkor viszlát! :'(");
             }
+
+            Console.WriteLine("Hallod, nesze itt van 100 csillag!");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.SetCursorPosition(
+                    rnd.Next(Console.WindowWidth),
+                    rnd.Next(Console.WindowHeight));
+                Console.ForegroundColor = (ConsoleColor)rnd.Next(1, 16);
+                Console.Write("*");
+            }
+
             Console.ReadKey();
         }
     }
